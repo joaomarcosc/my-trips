2482422cfed3f1cd27bb2bd45f72f3207d26795f
joaomarcos <j.marcos9@hotmail.com>
2021-08-18 11:06:53 -0300

    skdaoskao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
-
+const opa = 'ksoa'
 export default function Home() {
-  return <Map />
+  return (
+    <>
+      {opa}
+      <Map />
+    </>
+  )
 }
