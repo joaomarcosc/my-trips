bc8adac9b1ebda288890bc2550350025e887b1b6
joaomarcos <j.marcos9@hotmail.com>
2021-08-18 11:12:14 -0300

    fix: skdaoskao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
-const opa = 'ksoa'
 export default function Home() {
-  return (
-    <>
-      {opa}
-      <Map />
-    </>
-  )
+  return <Map />
 }
